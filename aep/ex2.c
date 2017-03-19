3637ce6b0ba1508fd6af4225e6f33bfca0d4e114
sourcered <source_red@outlook.com>
2017-03-19 18:11:08 -0300

    Fixing bugs

--- aep/ex2.c
+++ aep/ex2.c
@@ -5,7 +5,21 @@ void sortArrayAsc(int [], int);
 void concatArray(int [], int [], int[], int, int);
 
 #if 1
-#include "mylib.h"
+// #include "mylib.h"
+void printArray(int v[], int size)
+{
+  for(int i = 0; i < size; i++)
+    printf("%d ", v[i]);
+  printf("\n");
+}
+
+void printArrayFloat(float v[], int size)
+{
+  for(int i = 0; i < size; i++)
+    printf("%.2f\n", v[i]);
+  printf("\n");
+}
+
 #endif
 
 int main()
--- aep/ex3.c
+++ aep/ex3.c
@@ -1,7 +1,21 @@
 #include "stdio.h"
 
 #if 1
-#include "mylib.h"
+// #include "mylib.h"
+void printArray(int v[], int size)
+{
+  for(int i = 0; i < size; i++)
+    printf("%d ", v[i]);
+  printf("\n");
+}
+
+void printArrayFloat(float v[], int size)
+{
+  for(int i = 0; i < size; i++)
+    printf("%.2f\n", v[i]);
+  printf("\n");
+}
+
 #endif
 
 typedef struct
